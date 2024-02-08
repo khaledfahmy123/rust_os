8e85bfd464d59be7b4ed0379f1d506b3dda8a989
savitar <khaledfahmy345@gmail.com>
2024-02-08 13:35:00 +0200

    boots on qemu 'Hello world'

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,20 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+static HELLO: &[u8] = b"Hello World!";
 
+
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop{}
+
 }
